6ed0424cd326649abda31e736ee14212ceeb53e0
JenilMehta31 <jenil_mehta@Jenil-OmniBook.localdomain>
2026-06-27 11:15:13 +0000

    Fix app.js

--- app.js
+++ app.js
@@ -33,9 +33,6 @@ app.use(express.static(path.join(__dirname,"public")))
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    crypto: {
-        secret : process.env.SECRET 
-    },
     touchAfter: 24*60*60,
 });
 
